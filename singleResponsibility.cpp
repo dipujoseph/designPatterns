1a640838ae3c4f9cbd80a1caf326bb17fb1d42ce
Dipu <xyz@gmail.com>
2022-07-29 18:56:37 +0530

    Changed  boost lexical cast to std::to_string. Added comment

--- singleResponsibility.cpp
+++ singleResponsibility.cpp
@@ -2,9 +2,17 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <boost/lexical_cast.hpp>
 using namespace std;
+/*
+SOLID : First one -Single responsibility principle
+Real life example :  Code developer should not be handling office stationery management
+Code example
+ Jounnal entry class should handle only add, removal of entries
+ Saving/Persistance should not be handled. Reason
+1. There might be other classes also which need to be saved. So this could be managed by a seperate class e.g Persistance Manager :) 
+2.May decide to save to database instead of file. All class would have to undergo change
 
+*/
 struct Journal
 {
   string title;
@@ -24,7 +32,7 @@ struct Journal
 void Journal::add(const string& entry)
 {
   static int count = 1;
-  entries.push_back(boost::lexical_cast<string>(count++)
+  entries.push_back(std::to_string(count++)
     + ": " + entry);
 }
 
@@ -32,7 +40,10 @@ void Journal::save(const string& filename)
 {
   ofstream ofs(filename);
   for (auto& s : entries)
+  {
     ofs << s << endl;
+    cout <<s <<endl;
+  }
 }
 
 struct PersistenceManager
@@ -40,12 +51,15 @@ struct PersistenceManager
   static void save(const Journal& j, const string& filename)
   {
     ofstream ofs(filename);
-    for (auto& s : j.entries)
+    for (auto &s : j.entries)
+    {
       ofs << s << endl;
+      cout << s;
+    }
   }
 };
 
-void main()
+int main()
 {
   Journal journal{"Dear Diary"};
   journal.add("I ate a bug");
